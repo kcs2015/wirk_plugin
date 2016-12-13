76db9c7c99cbb177e454ad53fd1d9340162fbfa2
kcs2015 <knightclientsolutions@gmail.com>
2016-12-13 01:44:57 -0500

    Added WP Plugin header

---
+++ wirk-plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Plugin Name: Who Is Richard Knight Plugin
+Plugin URI: https://whoisrichardknight.com/
+Description: Does great things
+Version: 0.1.0
+Author: Richard Knight
+Author URI: https://whoisrichardknight.com/
+Text Domain: wirk-plugin
+
+*/
+?>
