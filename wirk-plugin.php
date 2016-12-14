a4b8e87c9dd22481782fee42765613b099c79d78
kcs2015 <knightclientsolutions@gmail.com>
2016-12-14 10:08:36 -0500

    Added initial plugin functions
    Create demo sample post type
    activation register plugin hook
    deactivate plugin hook

--- wirk-plugin.php
+++ wirk-plugin.php
@@ -10,4 +10,41 @@ Author URI: https://whoisrichardknight.com/
 Text Domain: wirk-plugin
 
 */
+
+/**
+ *
+ */
+function wirkplugin_activate() {
+
+    create_demo_sample_posttype();
+}
+register_activation_hook( __FILE__, 'wirkplugin_activate' );
+
+// Our custom post type function
+function create_demo_sample_posttype() {
+
+    register_post_type( 'demo_sample',
+        // CPT Options
+        array(
+            'labels' => array(
+                'name' => __( 'Demo Sample' ),
+                'singular_name' => __( 'Demo Samples' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array('slug' => 'demo_sample'),
+        )
+    );
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_demo_sample_posttype' );
+
+
+function remove_demo_sample_posttype(){
+    unregister_post_type("demo_sample");
+}
+
+register_deactivation_hook(__FILE__, 'remove_demo_sample_posttype');
+
+//register_uninstall_hook( );
 ?>
