7901285ce67d6040c27a25cc511348097292b213
kcs2015 <knightclientsolutions@gmail.com>
2016-12-22 10:07:14 -0500

    Added Update and Display meta data functions to custom post

--- wirk-plugin.php
+++ wirk-plugin.php
@@ -73,8 +73,11 @@ add_action( 'init', 'demo_sample_taxonomies', 0 );
 
 // ADD Metaboxes
 
-add_action( 'add_meta_boxes', 'demo_sample_description_box' );
-function demo_sample_description_box() {
+add_action( 'add_meta_boxes', 'demo_sample_meta_boxes',10, 2 );
+function demo_sample_meta_boxes($p_type, $curr_post) {
+    // Get Saved Meta
+    $GLOBALS["saved_demo_meta"] = get_post_meta( $curr_post->ID );
+
     add_meta_box(
         'demo_description_box',
         __( 'Demo Description', 'myplugin_textdomain' ),
@@ -101,30 +104,109 @@ function demo_sample_description_box() {
         'side',
         'low'
     );
-
-
 }
 
 function demo_description_box_content( $post ) {
-    wp_nonce_field( plugin_basename( __FILE__ ), 'demo_description_box_nonce' );
 
-    echo 'Enter a quick description of what the demo sample does: <br><textarea rows="4" cols="100">
-At w3schools.com you will learn how to make a website. We offer free tutorials in all web development technologies. 
-</textarea>';
+    wp_nonce_field( 'update_demo_description', 'demo_description_box_nonce' );
+
+    // Get Saved Meta
+    if (isset($GLOBALS["saved_demo_meta"]['demo_description'][0])){
+        $saved_demo_description = $GLOBALS["saved_demo_meta"]['demo_description'][0] ;
+    }
+
+    echo 'Enter a quick description of what the demo sample does: <br><textarea id="demo_description" name="demo_description" rows="4" cols="100">' .$saved_demo_description .'</textarea>';
 }
 function demo_instruction_box_content( $post ) {
-    wp_nonce_field( plugin_basename( __FILE__ ), 'demo_instruction_box_nonce' );
 
-    echo 'Enter instructions explaining to the the visitor how use the demo: <br><textarea rows="4" cols="100">
- 
+    wp_nonce_field( 'update_demo_instruction', 'demo_instruction_box_nonce' );
+
+    //Get Saved Meta
+    if (isset($GLOBALS["saved_demo_meta"]['demo_instruction'][0])){
+        $saved_demo_instruction = $GLOBALS["saved_demo_meta"]['demo_instruction'][0] ;
+    } else{
+        $saved_demo_instruction ='';
+    }
+
+  echo 'Enter instructions explaining to the the visitor how use the demo: <br><textarea id="demo_instruction" name="demo_instruction" rows="4" cols="100">' .$saved_demo_instruction .' 
 </textarea>';
 }
 function demo_shortcode_box_content( $post ) {
-    wp_nonce_field( plugin_basename( __FILE__ ), 'demo_shortcode_box_nonce' );
+    wp_nonce_field( 'update_demo_shortcode', 'demo_shortcode_box_nonce' );
 
-    echo 'Create a shortcode id for this the demo sample (i.e. - sku-cat-page-insert): <br><label for="demo_description"></label>';
-    echo '<input type="text" id="demo_description" name="demo_description" placeholder="Enter the shortcode" />';
+    //Get Saved Meta
+    if (isset($GLOBALS["saved_demo_meta"]['demo_shortcode'][0])){
+        $saved_demo_shortcode = $GLOBALS["saved_demo_meta"]['demo_shortcode'][0] ;
+    }else{
+        $saved_demo_shortcode ='';
+    }
+
+    echo 'Create a shortcode id for this the demo sample (i.e. - sku-cat-page-insert): <br><label for="demo_shortcode"></label>';
+    echo '<input type="text" id="demo_shortcode" name="demo_shortcode" placeholder="Enter the shortcode" value="' .$saved_demo_shortcode .'" />';
 }
 
+/**
+ * Save post metadata when a post is saved.
+ *
+ * @param int $post_id The post ID.
+ * @param post $post The post object.
+ * @param bool $update Whether this is an existing post being updated or not.
+ */
+function demo_sample_save_meta( $post_id, $post, $update ) {
 
+    /*
+     * In production code, $slug should be set only once in the plugin,
+     * preferably as a class property, rather than in each function that needs it.
+     */
+    $curr_post_type = get_post_type($post_id);
+
+    // If this isn't a 'demo_sample' post, don't update it.
+   if ( 'demo_sample' == $curr_post_type ) {
+
+
+        if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+            return;
+
+       if(isset($_POST['demo_description_box_nonce'])) {
+           if (wp_verify_nonce($_POST['demo_description_box_nonce'], 'update_demo_description')) {
+               // - Update the post's metadata.
+               update_post_meta($post_id, 'demo_description', sanitize_text_field($_POST['demo_description']));
+           }
+       }
+       if(isset($_POST['demo_instruction_box_nonce'])) {
+           if (wp_verify_nonce($_POST['demo_instruction_box_nonce'], 'update_demo_instruction')) {
+               // - Update the post's metadata.
+               update_post_meta($post_id, 'demo_instruction', sanitize_text_field($_POST['demo_instruction']));
+           }
+       }
+       if(isset($_POST['demo_shortcode_box_nonce'])) {
+           if (wp_verify_nonce($_POST['demo_shortcode_box_nonce'], 'update_demo_shortcode')) {
+               // - Update the post's metadata.
+               update_post_meta($post_id, 'demo_shortcode', sanitize_text_field($_POST['demo_shortcode']));
+           }
+       }
+           /* if (isset($_POST['demo_description'])) {
+                update_post_meta($post_id, 'demo_description', sanitize_text_field($_POST['demo_description']));
+            }
+        */
+
+
+    }   // END IF - DEMO SAMPLE
+
+    /*if ( 'page' == $curr_post_type ) {
+        if ( !current_user_can( 'edit_page', $post_id ) )
+            return;
+    } else {
+        if ( !current_user_can( 'edit_post', $post_id ) )
+            return;
+    }*/
+
+   /* $product_price = $_POST['product_price'];
+update_post_meta( $post_id, 'product_price', $product_price );*/
+
+
+} // END FUNCTION
+
+
+add_action( 'save_post', 'demo_sample_save_meta', 10, 3 );
 ?>
